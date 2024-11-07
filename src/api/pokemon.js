5177cb6dc83cf9c93bab5d86857189a6b599c1d7
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-11-07 15:42:36 -0600

    Pokémon Information

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -7,7 +7,17 @@ export async function getPokemonApi() {
     const result = await response.json();
     return result;
   } catch (error) {
-    throw (error);
+    throw error;
   }
 
 };
+
+export async function getPokemonDetailsByUrlApi(url) {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,9 +1,10 @@
-import react, { useEffect } from "react";
+import react, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { getPokemonApi } from "../api/pokemon";
+import { getPokemonApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 function Pokedex() {
+  const [ pokemons, setPokemons ] = useState([]);
 
   useEffect(() => {
     // funcion anomima autoejecutable () ()
@@ -15,8 +16,22 @@ function Pokedex() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonApi();
-      console.log(response);
       
+      const pokemonArray = [];
+
+      for await (const pokemon of response.results){
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+
+        pokemonArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          image: pokemonDetails.sprites.other["official-artwork"].front_default,
+        })
+        
+      }
+      setPokemons([...pokemons, ...pokemonArray]);
     } catch (error) {
       console.error(error);
     }
