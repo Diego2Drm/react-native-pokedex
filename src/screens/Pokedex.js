3d88cd44aece15b2d479a178dfa29c44004f573a
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-11-07 11:25:11 -0600

    API fetch

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,28 @@
-import react from "react";
+import react, { useEffect } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { getPokemonApi } from "../api/pokemon";
 
 function Pokedex() {
+
+  useEffect(() => {
+    // funcion anomima autoejecutable () ()
+    ( async () => {
+      await loadPokemons();
+    })();
+  } ,[]);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonApi();
+      console.log(response);
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
     return(
       <SafeAreaView>
         <Text>Pokedex</Text>
