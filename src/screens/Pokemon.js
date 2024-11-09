840fade979788de5a717aaceeed481c2a75b6b00
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-11-08 21:27:22 -0600

    Pokemon details API

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { getPokemonDetailsApi } from "../api/pokemon";
 
 function Pokemon(props) {
-const { navigation , route } = props;
-  console.log(route);
+  const { navigation, route: { params }, } = props;
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if(!pokemon) return null;
   
-    return(
+    return (
       <View>
-        <Text>Pokemon</Text>
+        <Text>Detalles Pokemon</Text>
+        <Text>{pokemon.name}</Text>
+        <Image source={{ uri: pokemon.sprites.other["official-artwork"].front_default }} style={styles.image}/>
       </View>
     );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 90,
+    height: 90,
+  },
+})
 
 export { Pokemon }
