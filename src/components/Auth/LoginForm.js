2a0a0f265cdd6dd6478922b88f398b72b2ca4ddb
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-11-11 20:28:46 -0600

    Implementing Formik and Yup

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,25 +1,58 @@
 import React from "react";
 import { Button } from "react-native";
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log("Formulario enviado...");
+      console.log(formValue);
+    },
+  });
+
   return (
     <View>
-      <Text style={styles.title}>Iniciar Sesión</Text>
+      <Text style={styles.title}>Iniciar sesión</Text>
       <TextInput
-        placeholder="Nombre del Usuario"
+        placeholder="Nombre de usuario"
         style={styles.input}
         autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
       />
       <TextInput
         placeholder="Contraseña"
         style={styles.input}
         autoCapitalize="none"
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
       />
-      <Button title="Ingresar" onPress={() => alert("Ingresando...")} />
+      <Button title="Entrar" onPress={formik.handleSubmit} />
+
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   );
+}
+
+function initialValues() {
+  return {
+    username: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required("El usuario es obligatorio"),
+    password: Yup.string().required("La contraseña es obligatoria"),
+  };
 }
 
 const styles = StyleSheet.create({
@@ -37,4 +70,12 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
   },
+  error: {
+    textAlign: "center",
+    color: "#f00",
+    marginTop: 20,
+  },
 });
+
+
+// yarn add formik yup
