000fc2d9247da889143463ab80eeed58068bb638
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-11-11 20:12:06 -0600

    Login Form

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Button } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm....</Text>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput
+        placeholder="Nombre del Usuario"
+        style={styles.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+      <Button title="Ingresar" onPress={() => alert("Ingresando...")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
